3d35db2b591009617f696d3c122ffcb8eb36f935
hinest <treverhines@gmail.com>
2015-10-22 10:13:11 -0400

    commit before pull

--- rbf/testtopo.py
+++ rbf/testtopo.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python
 import numpy as np
-from spect.halton import Halton
-from spectral.topology import Domain
+from rbf.halton import Halton
 import matplotlib.pyplot as plt
-import pyximport;pyximport.install()
-import nodes
+import rbf.spatial as spatial
 import modest
 
 def curve(t):
   return np.array([0.7 + 0.2*np.cos(t),0.5 + 0.2*np.sin(t)])
 
-points = np.array([curve(i) for i in np.linspace(0,2*np.pi,5)])
+points = np.array([curve(i) for i in np.linspace(0,2*np.pi,1000)])
 print(np.shape(points))
 points2 = np.array([points])
 
 
-D = Domain(points2,['main'])
 H = Halton(2)
-N = 10000
-seq = H(N)
+N = 1000000
 modest.tic()
-d = nodes.contains(points,seq)
+seq = H(N)
+print(modest.toc())
+
+modest.tic()
+d = spatial.contains(points,seq)
 print(modest.toc())
 
 #modest.tic()
