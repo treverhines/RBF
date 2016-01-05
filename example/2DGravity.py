fec1dcabc7ed093c0d74b37db7ec22f56d982725
hinest <treverhines@gmail.com>
2016-01-04 21:32:46 -0500

    added gravity example

---
+++ example/2DGravity.py
@@ -0,0 +1,272 @@
+#!/usr/bin/env python
+import numpy as np
+import rbf.nodegen
+from rbf.basis import mq as basis
+from rbf.normalize import normalizer
+from rbf.geometry import boundary_contains
+import modest
+from rbf.weights import rbf_weight
+import rbf.stencil
+from rbf.formulation import coeffs_and_diffs
+from rbf.formulation import evaluate_coeffs_and_diffs
+import numpy as np
+import rbf.bspline
+from myplot.cm import slip2
+import matplotlib.pyplot as plt
+import scipy.sparse
+import scipy.sparse.linalg
+import logging
+from modest import summary
+import sympy as sp
+from elastostatics import okada
+logging.basicConfig(level=logging.INFO)
+
+
+@modest.funtime
+def solver(G,d):
+  if not scipy.sparse.isspmatrix_csc(G):
+    G = scipy.sparse.csc_matrix(G)
+
+  # sort G and d using reverse cuthill mckee algorithm
+  perm = scipy.sparse.csgraph.reverse_cuthill_mckee(G)  
+  rev_perm = np.argsort(perm)
+  G = G[perm,:]
+  G = G[:,perm]
+  d = d[perm]
+
+  # form jacobi preconditioner
+  diag = np.array(G[range(G.shape[0]),range(G.shape[0])])[0]
+  if np.any(diag == 0.0):
+    raise ValueError(
+      'matrix cannot be sorted into a diagonal dominant matrix')
+
+  M = scipy.sparse.diags(1.0/diag,0)
+  out,status = scipy.sparse.linalg.lgmres(G,d,M=M,maxiter=1)
+  if status != 0:
+    print('lgmres exited with status %s, trying direct solve' % status)
+    out = scipy.sparse.linalg.spsolve(G,d)
+
+  # return to original sorting
+  d = d[rev_perm]
+  out = out[rev_perm]
+  G = G[rev_perm,:]
+  G = G[:,rev_perm]
+  return out   
+
+
+# formulate the PDE
+x = sp.symbols('x0:2')
+n = sp.symbols('n0:2')
+u = (sp.Function('u0')(*x),
+     sp.Function('u1')(*x))
+L = sp.Function('L')(*x)
+M = sp.Function('M')(*x)
+
+dim = 2
+F = [[u[i].diff(x[j]) for i in range(dim)] for j in range(dim)]
+F = sp.Matrix(F)
+strain = sp.Rational(1,2)*(F + F.T)
+stress = L*sp.eye(dim)*sp.trace(strain) + 2*M*strain
+PDEs = [sum(stress[i,j].diff(x[j]) for j in range(dim)) for i in range(dim)]
+FreeBCs = [sum(stress[i,j]*n[j] for j in range(dim)) for i in range(dim)]
+FixBCs = [u[i] for i in range(dim)]
+
+def lamb(i):
+  if (nodes[i,1] > 0.05):
+    return 1.0#2.0
+  if (nodes[i,1] <= 0.05) & (nodes[i,1] >= -0.05):
+    return 1.0#2.0 - 1.8*(0.05 - nodes[i,1])/0.1
+  if (nodes[i,1] < -0.05):
+    return 1.0#0.2
+
+def lamb_diffy(i):
+  if (nodes[i,1] > 0.05):
+    return 0.0
+  if (nodes[i,1] <= 0.05) & (nodes[i,1] >= -0.05):
+    return 0.0#1.8/0.05
+  if (nodes[i,1] < -0.05):
+    return 0.0
+
+# norms is an array which is later defined
+sym2num = {L:1.0,
+           M:1.0,
+           L.diff(x[0]):0.0,
+           L.diff(x[1]):lamb_diffy,
+           M.diff(x[0]):0.0,
+           M.diff(x[1]):lamb_diffy,
+           n[0]:lambda i:norms[i,0],
+           n[1]:lambda i:norms[i,1]}
+
+DiffOps = [[coeffs_and_diffs(PDEs[i],u[j],x,mapping=sym2num) for j in range(dim)] for i in range(dim)]
+FreeBCOps = [[coeffs_and_diffs(FreeBCs[i],u[j],x,mapping=sym2num) for j in range(dim)] for i in range(dim)]
+FixBCOps = [[coeffs_and_diffs(FixBCs[i],u[j],x,mapping=sym2num) for j in range(dim)] for i in range(dim)]
+
+cond=10
+# The number of nodes needed will depend entirely on how sharply slip varies
+N = 50000
+
+# Ns=7 produces fantastic results in 2D because it is the number of 
+# adjacent nodes assuming HCP.  but 7 can be dangerous if there is 
+# a really shitty mesh 9 is a safer bet 
+Ns = 9
+Np = 1
+
+# domain vertices
+
+surf_vert_x = np.linspace(-20,20,200)
+surf_vert_y = 1.0/(1 + 1.0*(surf_vert_x)**2)
+#surf_vert_y = 2.0*np.sin(4*np.pi*surf_vert_x/10.0)
+
+surf_vert = np.array([surf_vert_x,surf_vert_y+10]).T
+surf_smp = np.array([np.arange(199),np.arange(1,200)]).T
+bot_vert = np.array([[-20.0,-10.0],
+                     [20.0,-10.0]])
+vert = np.vstack((bot_vert,surf_vert))
+smp = np.concatenate(([[0,1],[0,2],[1,201]],surf_smp+2))
+smp = np.array(smp,dtype=int)
+
+#vert = np.array([[-20.0,-10.0],
+#                 [20.0,-10.0],
+#                 [20.0,10.0],
+#                 [2.0,10.0],
+#                 [0.0,11.0],
+#                 [-2.0,10.0],
+#                 [-20.0,10.0]])
+#smp = np.array([[0,1],[1,2],[2,3],[3,4],[4,5],[5,6],[6,0]])
+grp = 2*np.ones(len(smp))
+grp[[0,1,2]] = 1
+#for s in smp:
+#  plt.plot(vert[s,0],vert[s,1],'o-')
+#plt.show()
+# 1 = fixed
+# 2 = free
+
+#grp = np.array(grp,dtype=int)
+
+
+# density function
+@normalizer(vert,smp,kind='density',nodes=N)
+def rho(p):
+  out = 1.0/(1 + 1.1*np.linalg.norm(p - np.array([0.0,11.0]),axis=1)**2)
+  out += 1.0/(1 + 1.1*np.linalg.norm(p - np.array([2.0,10.0]),axis=1)**2)
+  out += 1.0/(1 + 1.1*np.linalg.norm(p - np.array([-2.0,10.0]),axis=1)**2)
+  return out
+
+scale = np.max(vert) - np.min(vert)
+
+# domain nodes
+nodes_d,norms_d,group_d = rbf.nodegen.volume(rho,vert,smp,groups=grp)
+
+
+nodes,ix = rbf.nodegen.merge_nodes(interior=nodes_d[group_d==0],
+                                   fixed=nodes_d[group_d==1],
+                                   free=nodes_d[group_d==2],
+                                   free_ghost=nodes_d[group_d==2])
+
+norms,ix = rbf.nodegen.merge_nodes(interior=norms_d[group_d==0],
+                                   fixed=norms_d[group_d==1],
+                                   free=norms_d[group_d==2],
+                                   free_ghost=norms_d[group_d==2])
+
+# find the nearest neighbors for the ghost nodes
+s,dx = rbf.stencil.nearest(nodes[ix['free_ghost']],nodes,3)
+
+# The closest nodes are going the be the free nodes, which currently 
+# are on top of the ghost nodes. find the distance to the next closest 
+# node
+dx = dx[:,[2]]
+
+# shift the ghost nodes outside        
+nodes[ix['free_ghost']] += dx*norms[ix['free_ghost']]
+plt.plot(nodes[:,0],nodes[:,1],'o')
+plt.show()
+
+# find the stencils and distances now that the ghost nodes have been
+# moved
+s,dx = rbf.stencil.nearest(nodes,nodes,Ns)
+
+eps = rbf.weights.shape_factor(nodes,s,basis,cond=cond,samples=200)
+
+N = len(nodes)
+modest.tic('forming G')
+
+G = [[scipy.sparse.lil_matrix((N,N),dtype=np.float64) for mi in range(dim)] for di in range(dim)]
+data = [np.zeros(N) for i in range(dim)]
+# This can be parallelized!!!!
+for di in range(dim):
+  for mi in range(dim):
+    # apply the PDE to interior nodes and free nodes
+    for i in ix['interior']+ix['free']:
+      w = rbf_weight(nodes[i],
+                     nodes[s[i]],
+                     evaluate_coeffs_and_diffs(DiffOps[di][mi],i),
+                     eps=eps[i],
+                     Np=Np,
+                     basis=basis,
+                     cond=cond)
+      G[di][mi][i,s[i]] = w
+
+    for i in ix['fixed']:
+      w = rbf_weight(nodes[i],
+                     nodes[s[i]],
+                     evaluate_coeffs_and_diffs(FixBCOps[di][mi],i),
+                     eps=eps[i],
+                     Np=Np,
+                     basis=basis,
+                     cond=cond)
+      G[di][mi][i,s[i]] = w
+
+    # use the ghost node rows to enforce the free boundary conditions
+    # at the boundary nodes
+    for itr,i in enumerate(ix['free_ghost']):
+      j = ix['free'][itr]
+      w = rbf_weight(nodes[j],
+                     nodes[s[j]],
+                     evaluate_coeffs_and_diffs(FreeBCOps[di][mi],j),
+                     eps=eps[j],
+                     Np=Np,
+                     basis=basis,
+                     cond=cond)
+      G[di][mi][i,s[j]] = w
+    
+
+G = [scipy.sparse.hstack(G[i]) for i in range(dim)]
+G = scipy.sparse.vstack(G)
+
+# add gravitational force
+data[1][ix['interior']] -= (nodes[ix['interior'],1] > 10.01).astype(float)
+data[1][ix['free']] -= (nodes[ix['free'],1] > 10.01).astype(float)
+data = np.concatenate(data)
+
+modest.toc('forming G')
+out = solver(G,data)
+out = np.reshape(out,(dim,N))
+fig,ax = plt.subplots()
+cs = ax.tripcolor(nodes[:,0],
+                  nodes[:,1],
+                  np.linalg.norm(out,axis=0),cmap=slip2)
+plt.colorbar(cs)
+plt.quiver(nodes[::2,0],nodes[::2,1],
+           out[0,::2],out[1,::2],color='k',scale=100)
+
+
+
+x = nodes[:,0]
+z = nodes[:,1] - 10.0
+y = 0 + 0*x
+pnts = np.array([x,y,z]).T
+
+#soln = okada.okada92(pnts,np.array([0.0,0.0,2.0]),np.array([0.0,-10.0,0.0]),20.0,1.0,0.0,np.pi/2.0)
+#solnint = soln[ix['interior'],:]
+#outint = out[:,ix['interior']].T
+#print(np.max(np.abs(solnint[:,[0,1]]-outint)))
+#plt.quiver(x[::2],z[::2]+10.0,soln[::2,0],soln[::2,2],color='b',scale=20.0)
+#plt.show()
+
+
+logging.basicConfig(level=logging.INFO)
+summary()
+
+plt.show()
+
+
