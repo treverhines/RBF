08597332352aeddeacadd7a1d588ba1d435a462c
hinest <treverhines@gmail.com>
2016-01-14 21:46:00 -0500

    added much more stable fault example. It is more stable because it uses polyharmonic splines and ghost nodes on the fault

--- example/2DFault.py
+++ example/2DFault.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import numpy as np
 import rbf.nodegen
-from rbf.basis import phs4 as basis
+from rbf.basis import phs3 as basis
 from rbf.normalize import normalizer
 from rbf.geometry import boundary_contains
 import modest
@@ -33,17 +33,8 @@ def solver(G,d):
   G = G[:,perm]
   d = d[perm]
 
-  # form jacobi preconditioner
-  diag = np.array(G[range(G.shape[0]),range(G.shape[0])])[0]
-  if np.any(diag == 0.0):
-    raise ValueError(
-      'matrix cannot be sorted into a diagonal dominant matrix')
-
-  M = scipy.sparse.diags(1.0/diag,0)
-  out,status = scipy.sparse.linalg.lgmres(G,d,M=M,maxiter=1)
-  if status != 0:
-    print('lgmres exited with status %s, trying direct solve' % status)
-    out = scipy.sparse.linalg.spsolve(G,d)
+  # it is not clear whether sorting the matrix helps much
+  out = scipy.sparse.linalg.spsolve(G,d)
 
   # return to original sorting
   d = d[rev_perm]
@@ -100,57 +91,33 @@ DiffOps = [[coeffs_and_diffs(PDEs[i],u[j],x,mapping=sym2num) for j in range(dim)
 FreeBCOps = [[coeffs_and_diffs(FreeBCs[i],u[j],x,mapping=sym2num) for j in range(dim)] for i in range(dim)]
 FixBCOps = [[coeffs_and_diffs(FixBCs[i],u[j],x,mapping=sym2num) for j in range(dim)] for i in range(dim)]
 
-# The number of nodes needed will depend entirely on how sharply slip varies
+# The number of nodes needed depends on how sharp slip varies on the fault
 N = 1000
-
-# Ns=7 produces fantastic results in 2D because it is the number of 
-# adjacent nodes assuming HCP.  but 7 can be dangerous if there is 
-# a really shitty mesh 9 is a safer bet 
-Ns = 20
-order = 'max'
+Ns = 50
+# It seems like using the maximum polynomial order is not helpful for
+# this problem. Stick with cubic order polynomials and RBFs 
+order = 4
 
 # domain vertices
-
-surf_vert_x = np.linspace(-10,10,100)
-surf_vert_y = 1.0/(1 + 1.0*(surf_vert_x - 2)**2)
-surf_vert_y = 0.2*np.sin(4*np.pi*surf_vert_x/10.0)
-
-surf_vert = np.array([surf_vert_x,surf_vert_y+12]).T
-surf_smp = np.array([np.arange(99),np.arange(1,100)]).T
-bot_vert = np.array([[-10.0,-10.0],
-                     [10.0,-10.0]])
-vert = np.vstack((bot_vert,surf_vert))
-smp = np.concatenate(([[0,1],[0,2],[1,101]],surf_smp+2))
-smp = np.array(smp,dtype=int)
-
+vert = np.array([[-1.0,-1.0],
+                 [1.0,-1.0],
+                 [1.0,1.0],
+                 [-1.0,1.0]])
+smp = np.array([[0,1],[1,2],[2,3],[3,0]])
 # 1 = fixed
 # 2 = free
-grp = 2*np.ones(102)
-grp[[0,1,2]] = 1
-grp = np.array(grp,dtype=int)
+grp = np.array([1,1,2,1])
 
 # fault vertices
-vert_f = np.array([[0.0,9.0],
-                   [0.0001,10.1]])
+vert_f = np.array([[0.0,-0.5],
+                   [0.0001,0.5]])
 smp_f =  np.array([[0,1]])
-
 
 # density function
 @normalizer(vert,smp,kind='density',nodes=N)
 def rho(p):
-  out = np.zeros(p.shape[0])
-  #for v in vert_f:
-  out +=  1.0/(1 + 20.*np.linalg.norm(p - np.array([0.0,9.0]),axis=-1)**2)
-  out +=  1.0/(1 + 20.*np.linalg.norm(p - np.array([0.0,9.1]),axis=-1)**2)
-  out +=  1.0/(1 + 20.*np.linalg.norm(p - np.array([0.0,9.2]),axis=-1)**2)
-  out +=  1.0/(1 + 20.*np.linalg.norm(p - np.array([0.0,9.3]),axis=-1)**2)
-  out +=  1.0/(1 + 20.*np.linalg.norm(p - np.array([0.0,9.4]),axis=-1)**2)
-  out +=  1.0/(1 + 20.*np.linalg.norm(p - np.array([0.0,9.5]),axis=-1)**2)
-  out +=  1.0/(1 + 20.*np.linalg.norm(p - np.array([0.0,9.6]),axis=-1)**2)
-  out +=  1.0/(1 + 20.*np.linalg.norm(p - np.array([0.0,9.7]),axis=-1)**2)
-  out +=  1.0/(1 + 20.*np.linalg.norm(p - np.array([0.0,9.8]),axis=-1)**2)
-  out +=  1.0/(1 + 20.*np.linalg.norm(p - np.array([0.0,9.9]),axis=-1)**2)
-  out +=  1.0/(1 + 20.*np.linalg.norm(p - np.array([0.0,10.0]),axis=-1)**2)
+  out = 1.0/(1 + 50*np.linalg.norm(p-np.array([0.0,0.5]),axis=1)**2)
+  out += 1.0/(1 + 50*np.linalg.norm(p-np.array([0.0,-0.5]),axis=1)**2)
   return out
 
 scale = np.max(vert) - np.min(vert)
@@ -165,82 +132,66 @@ norms_f = norms_f[is_inside]
 group_f = group_f[is_inside]
 
 # define slip
-slip = np.zeros((dim,len(nodes_f)))
-knots = np.linspace(9.0,10.0,4)
+slip = np.zeros((dim,len(nodes_f[group_f==0])))
+knots = np.linspace(-0.5,0.5,6)
 basis_no = rbf.bspline.basis_number(knots,2)
-slip[0,:] = -np.sum(rbf.bspline.bsp1d(nodes_f[:,1],knots,i,2) for i in range(basis_no))
-#slip[1,:] = -np.sum(rbf.bspline.bsp1d(nodes_f[:,1],knots,i,2) for i in range(basis_no))
-#slip[1,:] = rbf.bspline.bsp1d(nodes_f[:,1],-0.5+np.array([0.0,0.33,0.66,1.0]),0,2)
-#slip[0,:] = 1.0
-#slip[1,:] = 1.0
+slip[1,:] = np.sum(rbf.bspline.bsp1d(nodes_f[group_f==0,1],knots,i,2) for i in range(basis_no))
 
-# ensures that there is no slip on end nodes
-slip[0,group_f==1]=0
-slip[1,group_f==1]=0
-
-plt.plot(nodes_f[:,1],slip[1,:],'ro')
-plt.plot(nodes_f[:,1],slip[0,:],'bo')
+plt.plot(nodes_f[group_f==0,1],slip[1,:],'ro')
+plt.plot(nodes_f[group_f==0,1],slip[0,:],'bo')
 plt.show()
 
 # domain nodes
 nodes_d,norms_d,group_d = rbf.nodegen.volume(rho,vert,smp,groups=grp,
-                                             fix_nodes=nodes_f,n=4)
+                                             fix_nodes=nodes_f,itr=1000,delta=0.01,n=6)
 
 # split fault nodes into hanging wall and foot wall nodes
-nodes_fh = nodes_f + 1e-10*scale*norms_f
-nodes_ff = nodes_f - 1e-10*scale*norms_f
-norms_fh = np.copy(norms_f)
-norms_ff = np.copy(norms_f)
+# THROW AWAY THE END NODES!
+nodes_fh = nodes_f[group_f==0] + 1e-10*scale*norms_f[group_f==0]
+nodes_ff = nodes_f[group_f==0] - 1e-10*scale*norms_f[group_f==0]
+norms_fh = norms_f[group_f==0]
+norms_ff = norms_f[group_f==0]
 
 nodes,ix = rbf.nodegen.merge_nodes(interior=nodes_d[group_d==0],
                                    fixed=nodes_d[group_d==1],
                                    free=nodes_d[group_d==2],
                                    free_ghost=nodes_d[group_d==2],
-                                   fault_hanging=nodes_fh,
-                                   fault_foot=nodes_ff)
+                                   fault_hanging=nodes_fh[...],
+                                   fault_hanging_ghost=nodes_fh[...],
+                                   fault_foot=nodes_ff[...],
+                                   fault_foot_ghost=nodes_ff[...])
 
 norms,ix = rbf.nodegen.merge_nodes(interior=norms_d[group_d==0],
                                    fixed=norms_d[group_d==1],
                                    free=norms_d[group_d==2],
                                    free_ghost=norms_d[group_d==2],
-                                   fault_hanging=norms_fh,
-                                   fault_foot=norms_ff)
+                                   fault_hanging=norms_fh[...],
+                                   fault_hanging_ghost=norms_fh[...],
+                                   fault_foot=norms_ff[...],
+                                   fault_foot_ghost=norms_ff[...])
 
-# find the nearest neighbors for the ghost nodes
-s,dx = rbf.stencil.nearest(nodes[ix['free_ghost']],nodes,3,vert=vert_f,smp=smp_f)
-
-# The closest nodes are going the be the free nodes, which currently 
-# are on top of the ghost nodes. find the distance to the next closest 
-# node
-dx = dx[:,[2]]
-
-# shift the ghost nodes outside        
-nodes[ix['free_ghost']] += dx*norms[ix['free_ghost']]
-
-# find the stencils and distances now that the ghost nodes have been
-# moved
+# find the node stencils
 s,dx = rbf.stencil.nearest(nodes,nodes,Ns,vert=vert_f,smp=smp_f)
 
-# change stencils so that fault nodes, where traction forces are
-# estimated, do not include other fault nodes. This greatly improves
-# accuracy and reduces the odds of instability when slip has sharp
-# discontinuities.  Adding ghost nodes to the fault does not improve
-# the solution and may make it worse
-fault_indices = np.array(ix['fault_hanging']+ix['fault_foot'])
-fault_stencil = rbf.stencil.nearest(nodes[fault_indices],nodes,Ns-1,
-                                    vert=vert_f,smp=smp_f,
-                                    excluding=fault_indices)[0] 
-# make sure the stencil includes itself
-fault_stencil = np.hstack((fault_indices[:,None],fault_stencil))
-s[fault_indices] = fault_stencil
+# Create a list of distances to the next closest node. This is used to
+# determine how far ghost nodes need to be shifted
+dx_next_closest = dx[:,[2]]
+
+# shift the ghost nodes outside        
+nodes[ix['free_ghost']] += dx_next_closest[ix['free_ghost']]*norms[ix['free_ghost']]
+
+# move hanging wall ghost nodes towards the foot wall and vice versa
+nodes[ix['fault_hanging_ghost']] -= dx_next_closest[ix['fault_hanging_ghost']]*norms[ix['fault_hanging_ghost']]
+nodes[ix['fault_foot_ghost']] += dx_next_closest[ix['fault_foot_ghost']]*norms[ix['fault_foot_ghost']]
+
 
 plt.plot(nodes[:,0],nodes[:,1],'ko')
 plt.show()
 # view nodes
-#for i in boundary_indices:
+#for i in ix['fault_foot']:
 #  plt.plot(nodes[:,0],nodes[:,1],'ko')
 #  plt.plot(nodes[s[i],0],nodes[s[i],1],'bo')
-#  plt.plot(nodes[s[i][0],0],nodes[s[i][0],1],'ro')
+#  plt.plot(nodes[i,0],nodes[i,1],'ro')
 #  plt.show()
 
 N = len(nodes)
@@ -252,10 +203,19 @@ data = [np.zeros(N) for i in range(dim)]
 for di in range(dim):
   for mi in range(dim):
     # apply the PDE to interior nodes and free nodes
-    for i in ix['interior']+ix['free']:
+    for i in ix['interior']:
       w = rbf_weight(nodes[i],
                      nodes[s[i]],
                      evaluate_coeffs_and_diffs(DiffOps[di][mi],i),
+                     order=order,
+                     basis=basis)
+
+      G[di][mi][i,s[i]] = w
+
+    for i in ix['free']:
+      w = rbf_weight(nodes[i],
+                     nodes[s[i]],
+                     evaluate_coeffs_and_diffs(FreeBCOps[di][mi],i),
                      order=order,
                      basis=basis)
 
@@ -287,7 +247,27 @@ for di in range(dim):
       j = ix['free'][itr]
       w = rbf_weight(nodes[j],
                      nodes[s[j]],
-                     evaluate_coeffs_and_diffs(FreeBCOps[di][mi],j),
+                     evaluate_coeffs_and_diffs(DiffOps[di][mi],j),
+                     order=order,
+                     basis=basis)
+
+      G[di][mi][i,s[j]] = w
+
+    for itr,i in enumerate(ix['fault_hanging_ghost']):
+      j = ix['fault_hanging'][itr]
+      w = rbf_weight(nodes[j],
+                     nodes[s[j]],
+                     evaluate_coeffs_and_diffs(DiffOps[di][mi],j),
+                     order=order,
+                     basis=basis)
+
+      G[di][mi][i,s[j]] = w
+
+    for itr,i in enumerate(ix['fault_foot_ghost']):
+      j = ix['fault_foot'][itr]
+      w = rbf_weight(nodes[j],
+                     nodes[s[j]],
+                     evaluate_coeffs_and_diffs(DiffOps[di][mi],j),
                      order=order,
                      basis=basis)
 
@@ -315,32 +295,20 @@ G = [scipy.sparse.hstack(G[i]) for i in range(dim)]
 G = scipy.sparse.vstack(G)
 data = np.concatenate(data)
 
+idx_noghost = ix['interior'] + ix['free'] + ix['fixed'] + ix['fault_hanging'] + ix['fault_foot']
 modest.toc('forming G')
 out = solver(G,data)
 out = np.reshape(out,(dim,N))
 out[:,ix['fault_foot']] = out[:,ix['fault_foot']] - slip
 out[:,ix['fault_hanging']] = out[:,ix['fault_foot']] + 2*slip
 fig,ax = plt.subplots()
-cs = ax.tripcolor(nodes[:,0],
-                  nodes[:,1],
-                  np.linalg.norm(out,axis=0),cmap=slip2)
+cs = ax.tripcolor(nodes[idx_noghost,0],
+                  nodes[idx_noghost,1],
+                  np.linalg.norm(out[:,idx_noghost],axis=0),cmap=slip2)
 plt.colorbar(cs)
-plt.quiver(nodes[::2,0],nodes[::2,1],
-           out[0,::2],out[1,::2],color='k',scale=20.0)
+plt.quiver(nodes[idx_noghost[::1],0],nodes[idx_noghost[::1],1],
+           out[0,idx_noghost[::1]],out[1,idx_noghost[::1]],color='k',scale=20.0)
 
-
-
-x = nodes[:,0]
-z = nodes[:,1] - 10.0
-y = 0 + 0*x
-pnts = np.array([x,y,z]).T
-
-#soln = okada.okada92(pnts,np.array([0.0,0.0,2.0]),np.array([0.0,-10.0,0.0]),20.0,1.0,0.0,np.pi/2.0)
-#solnint = soln[ix['interior'],:]
-#outint = out[:,ix['interior']].T
-#print(np.max(np.abs(solnint[:,[0,1]]-outint)))
-#plt.quiver(x[::2],z[::2]+10.0,soln[::2,0],soln[::2,2],color='b',scale=20.0)
-#plt.show()
 
 
 logging.basicConfig(level=logging.INFO)
