f2ec025a47e33b4a6a699a4a8273c25cc0ff2544
hinest <treverhines@gmail.com>
2016-07-06 12:09:51 -0400

    changed demo file

--- demo/demo_basis.py
+++ demo/demo_basis.py
@@ -12,15 +12,23 @@ c = np.array([[-2.0],[0.0],[2.0]]) # RBF centers
 x = np.linspace(-5.0,5.0,1000)[:,None] # evaluate at these points
 
 soln = iq(x,c)
+soln_diff = iq(x,c,diff=(1,))
 
 fig,ax = plt.subplots(figsize=(6,4))
 ax.plot(x,soln)
 ax.set_xlim((-5.0,5.0))
-ax.set_ylim((0.0,1.1))
-ax.set_title('inverse quadratic radial basis functions')
+ax.set_title('inverse quadratic')
 ax.grid()
 fig.tight_layout()
-plt.savefig('figures/demo_basis.png')
+plt.savefig('figures/demo_basis_1.png')
+
+fig,ax = plt.subplots(figsize=(6,4))
+ax.plot(x,soln_diff)
+ax.set_xlim((-5.0,5.0))
+ax.set_title('inverse quadratic first derivative')
+ax.grid()
+fig.tight_layout()
+plt.savefig('figures/demo_basis_2.png')
 
 
 
