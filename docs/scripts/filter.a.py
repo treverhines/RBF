28ca32418ee94be8410e2a93310444771855dc86
Trever Hines <treverhines@gmail.com>
2016-11-22 17:07:35 -0500

    added more demos to docs

--- docs/scripts/filter.a.py
+++ docs/scripts/filter.a.py
@@ -8,25 +8,28 @@ user specified cutoff frequency.  In this example the cutoff frequency
 is set to 40.
 '''
 import numpy as np
-from scipy.misc import lena
+from scipy.misc import face
 from scipy.interpolate import NearestNDInterpolator
 import matplotlib.pyplot as plt
 from rbf.filter import filter
 np.random.seed(4)
 
-x = np.linspace(0.0,1.0,512)
-y = np.linspace(1.0,0.0,512)
+x = np.linspace(0.0,1.0,768)
+y = np.linspace(1.0,0.0,768)
 x,y = np.meshgrid(x,y)
 points = np.array([x.flatten(),y.flatten()]).T
-values = lena().flatten().astype(float)
-values /= 255.0 # normalize so that the max is 1.0
+values = np.linalg.norm(face(),axis=2)[:,256:].flatten()
+#values = lena().flatten().astype(float)
+values /= 441.7 # normalize so that the max is 1.0
 signal = NearestNDInterpolator(points,values)
 
 # interpolate Lena onto new observation points and add noise
-points_obs = np.random.normal(0.5,0.25,(100000,2))
-u_obs = signal(points_obs) + np.random.normal(0.0,0.25,100000)
+points_obs = np.random.normal(0.5,0.3,(100000,2))
+#points_obs = rbf.halton.halton(50000,2)  
+u_obs = signal(points_obs) + np.random.normal(0.0,0.2,100000)
 # find filtered solution
-soln,sigma = filter(points_obs,u_obs,cutoff=40,n=20)
+cutoff = 60.0
+soln,_ = filter(points_obs,u_obs,cutoff=cutoff,n=20,samples=0)
 
 # plot the observed and filtered results
 fig,ax = plt.subplots(2,1,figsize=(6,10))
@@ -34,12 +37,12 @@ ax[0].set_aspect('equal')
 ax[0].set_axis_bgcolor('blue')
 ax[0].set_xlim((0,1))
 ax[0].set_ylim((0,1))
-ax[0].set_title('Noisy Data')
+ax[0].set_title('noisy data')
 ax[1].set_aspect('equal')
 ax[1].set_axis_bgcolor('blue')
 ax[1].set_xlim((0,1))
 ax[1].set_ylim((0,1))
-ax[1].set_title(u'Filtered Solution $\mathregular{(\omega_c = 40)}$')
+ax[1].set_title(u'filtered solution $\mathregular{(\omega_c = %s)}$' % cutoff)
 p1 = ax[0].scatter(points_obs[:,0],points_obs[:,1],s=4,c=u_obs,
                    edgecolor='none',cmap='Greys_r',vmin=-0.1,vmax=1.1)
 plt.colorbar(p1,ax=ax[0])
--- docs/scripts/filter.c.py
+++ docs/scripts/filter.c.py
@@ -1,10 +1,11 @@
 ''' 
-This script demonstrates smoothing and interpolating two-dimensional 
-data when there is a known discontinuity in the underlying signal.
+This script demonstrates smoothing 2-d data when there is a known 
+discontinuity in the underlying signal.
 '''
 import numpy as np
 import matplotlib.pyplot as plt
 from rbf.filter import filter
+import rbf.halton
 np.random.seed(1)
 
 def signal(x):
@@ -18,56 +19,36 @@ def signal(x):
 # define the known discontinuity
 bnd_vert = np.array([[0.0, 2.0],[0.0,-2.0]])
 bnd_smp = np.array([[0,1]])                     
-
-N = 200 # number of observations
-pnts_obs = np.random.uniform(-4,4,(N,2))
-# create observations with added white noise
-u_obs = signal(pnts_obs) + np.random.normal(0.0,0.1,N)
-sigma_obs = 0.1*np.ones(N)
-
-# create the interpolation points
-vals = np.linspace(-4,4,100) 
-pnts_itp = np.reshape(np.meshgrid(vals,vals),(2,10000)).T
-
-# rbf.filter.filter can be used for interpolation by treating the 
-# interpolation points as observation points with infinite 
-# uncertainty. This is done by concatenating pnts_obs and pnts_itp and 
-# then appending observations with infinite uncertainty to u_obs
-pnts = np.concatenate((pnts_obs,pnts_itp),axis=0)
-
-u_itp = np.empty(10000) + np.nan
-sigma_itp = np.empty(10000) + np.inf
-
-u = np.concatenate((u_obs,u_itp))
-sigma = np.concatenate((sigma_obs,sigma_itp))
-
-# cutoff frequency. Because we are interpolating data, there will not 
-# be a homogeneous characteristic wavelength for the solution. 
-# Interpolation points which do not have nearby observations will tend 
-# to have a longer wavelength.  The cutoff frequency should be 
-# interpretted as a rough estimate of the highest frequency allowed in 
-# the final solution.
-cutoff = 0.2
-
-soln = filter(pnts,u,sigma=sigma,cutoff=cutoff,vert=bnd_vert,smp=bnd_smp)
-
-# plot the results
+# create synthetic data
+pnts_obs = 8*(rbf.halton.halton(200,2,start=1) - 0.5)
+sigma_obs = 0.2*np.ones(200)
+u_obs = signal(pnts_obs) + np.random.normal(0.0,sigma_obs)
+# find the filtered solution
+cutoff = 0.5
+soln,_ = filter(pnts_obs,u_obs,sigma=sigma_obs,cutoff=cutoff,
+                vert=bnd_vert,smp=bnd_smp,check_all_edges=True)
+# plot the results and true signal
+vals = np.linspace(-4,4,200) 
+grid = np.reshape(np.meshgrid(vals,vals),(2,200**2)).T
 fig,axs = plt.subplots(2,1,figsize=(6,10))
-p = axs[0].tripcolor(pnts[:,0],pnts[:,1],soln[0],vmin=-1.0,vmax=1.0,cmap='viridis')
-axs[0].scatter(pnts_obs[:,0],pnts_obs[:,1],c=u_obs,s=50,vmin=-1.0,vmax=1.0,cmap='viridis')
-axs[0].plot(bnd_vert[:,0],bnd_vert[:,1],'r-',lw=6)
-axs[0].set_xlim((-4,4))
-axs[0].set_ylim((-4,4))
+axs[0].scatter(pnts_obs[:,0],pnts_obs[:,1],c=u_obs,s=50,
+               vmin=-1.0,vmax=1.0,cmap='viridis',zorder=1)
+p = axs[0].tripcolor(grid[:,0],grid[:,1],signal(grid),
+                     vmin=-1.0,vmax=1.0,cmap='viridis',zorder=0)
+axs[0].plot(bnd_vert[:,0],bnd_vert[:,1],'r-',lw=4)
+axs[0].set_xlim((-4,4));axs[0].set_ylim((-4,4))
 axs[0].set_aspect('equal')
-axs[0].set_title(u'Observed and Filtered Solution. $(\omega_c=%s)$' % cutoff)
+axs[0].set_title(u'observed data and true signal')
 plt.colorbar(p,ax=axs[0])
-p = axs[1].tripcolor(pnts[:,0],pnts[:,1],signal(pnts),vmin=-1.0,vmax=1.0,cmap='viridis')
-axs[1].plot(bnd_vert[:,0],bnd_vert[:,1],'r-',lw=6)
-axs[1].set_xlim((-4,4))
-axs[1].set_ylim((-4,4))
-axs[1].set_title('True Signal')
+axs[1].scatter(pnts_obs[:,0],pnts_obs[:,1],c=soln,s=50,
+               vmin=-1.0,vmax=1.0,cmap='viridis')
+p = axs[1].tripcolor(grid[:,0],grid[:,1],signal(grid),
+                     vmin=-1.0,vmax=1.0,cmap='viridis',zorder=0)
+axs[1].plot(bnd_vert[:,0],bnd_vert[:,1],'r-',lw=4)
+axs[1].set_xlim((-4,4));axs[1].set_ylim((-4,4))
 axs[1].set_aspect('equal')
+axs[1].set_title(r'filtered data and true signal ($\mathregular{\omega_c=%s}$)' % cutoff)
 plt.colorbar(p,ax=axs[1])
-#plt.savefig('../figures/filter.c.png')
 plt.tight_layout()
+plt.savefig('../figures/filter.c.png')
 plt.show()
