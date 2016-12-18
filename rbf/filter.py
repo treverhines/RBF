c9cec3386a661f88038f73c0ca2cc0a3e693c4c6
hinest <treverhines@gmail.com>
2016-12-18 12:55:11 -0500

    bug fix

--- rbf/filter.py
+++ rbf/filter.py
@@ -239,7 +239,7 @@ def filter(x,u,sigma=None,
   u = np.asarray(u)  
   u = np.nan_to_num(u)
   N,dim = x.shape
-  P = int(np.prod(u.shape[:-1])) 
+  Q = int(np.prod(u.shape[:-1])) 
   if sigma is None:
     sigma = np.ones(u.shape)
 
@@ -251,8 +251,8 @@ def filter(x,u,sigma=None,
 
   # flatten u and sigma to a 2D array
   input_u_shape = u.shape
-  u = u.reshape((P,N))
-  sigma = sigma.reshape((P,N))
+  u = u.reshape((Q,N))
+  sigma = sigma.reshape((Q,N))
     
   # memoized function to form the differentiation matrices used for 
   # the prior and post-processing
@@ -265,7 +265,7 @@ def filter(x,u,sigma=None,
     # and so it may take up a lot of memory if it is called many times 
     # with different masks
     mask = np.asarray(mask,dtype=bool)        
-    prior_diffs = order*np.eye(dim,dtype=int)
+    prior_diffs = p*np.eye(dim,dtype=int)
     L = rbf.fd.weight_matrix(x[~mask],x[~mask],prior_diffs,**kwargs)
     D = rbf.fd.weight_matrix(x[~mask],x[~mask],diffs,**kwargs)
     return L,D  
@@ -285,9 +285,9 @@ def filter(x,u,sigma=None,
     # form weight matrix
     W = _diag(1.0/sigma[i,~mask])
     # compute penalty parameter
-    p = _penalty(cutoff,order,sigma[i,~mask])
+    lamb = _penalty(cutoff,p,sigma[i,~mask])
     # form left and right hand side of the system to solve
-    lhs = W.T.dot(W) + L.T.dot(L)/p**2
+    lhs = W.T.dot(W) + L.T.dot(L)/lamb**2
     rhs = W.T.dot(W).dot(u[i,~mask])
     # generate LU decomposition of left-hand side
     lu = spla.splu(lhs)
@@ -307,7 +307,7 @@ def filter(x,u,sigma=None,
         w1 = np.random.normal(0.0,1.0,K)
         w2 = np.random.normal(0.0,1.0,K)
         # generate sample of the posterior
-        post_sample = lu.solve(rhs + W.T.dot(w1) + L.T.dot(w2)/p)
+        post_sample = lu.solve(rhs + W.T.dot(w1) + L.T.dot(w2)/lamb)
         # differentiate the sample
         post_sample = D.dot(post_sample)
         ivar.add_sample(post_sample)
@@ -324,7 +324,7 @@ def filter(x,u,sigma=None,
   # Calculate the posterior for each (N,) array in u and sigma. 
   # This is done in parallel, where *procs* is the number of 
   # subprocesses spawned.
-  post = rbf.mp.parmap(calculate_posterior,range(P),workers=procs)
+  post = rbf.mp.parmap(calculate_posterior,range(Q),workers=procs)
   post_mean = np.array([k[0] for k in post])
   post_sigma = np.array([k[1] for k in post])
 
