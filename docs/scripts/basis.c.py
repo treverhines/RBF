a77b4a5826c9546a07bcfb7705ec062be8381580
Trever Hines <treverhines@gmail.com>
2017-04-03 11:14:41 -0400

    added ad-hoc fix to matern example. Function is now correctly computed for r=0

--- docs/scripts/basis.c.py
+++ docs/scripts/basis.c.py
@@ -2,23 +2,28 @@
 In this script we define and plot a Matern RBF.
 '''
 import numpy as np
-from sympy import besselk
+from sympy import besselk,Piecewise
 from scipy.special import gamma
 import matplotlib.pyplot as plt
 from rbf.basis import RBF,get_r,get_eps
+from rbf.gauss import gpiso
+from scipy.signal import periodogram
 
 def make_matern(nu):
   '''returns an instance of a Matern RBF with order *nu*'''
+  tol = 1e-10
   r,eps = get_r(),get_eps() # get symbolic variables
   coeff = 2**(1 - nu)/gamma(nu) * (np.sqrt(2*nu)*r/eps)**nu
   expr  = coeff*besselk(nu,np.sqrt(2*nu)*r/eps)
+  # Handle a removable singularity at r=0 by setting it to 1. 
+  expr  = Piecewise((1.0,r<tol),(expr,True))
   return RBF(expr)
 
 x = np.linspace(0.0,3.0,500)[:,None]
 center = np.array([[0.0]])
 
 fig,ax = plt.subplots()
-for nu in [0.5,1.5,2.5]:
+for nu in [0.5,1.0,2.0]:
   mat = make_matern(nu)
   ax.plot(x,mat(x,center),label='$\\nu = $%s' % str(nu))
   
