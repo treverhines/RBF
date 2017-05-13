119ec0d8037d559559e8582c4f0e4d39fc3ae6f6
Trever Hines <treverhines@gmail.com>
2017-05-13 13:07:40 -0400

    improved sparse demo

--- docs/scripts/gauss.i.py
+++ docs/scripts/gauss.i.py
@@ -2,11 +2,9 @@
 This script demonstrates how the *GaussianProcess* class is smart
 enough to take advantage of sparse covariance function. The *gppoly*
 and *gpbfci* constructors returns a *GaussianProcess* with a sparse
-(entirely zero) covariance function. Conditioning the resulting
-*GaussianProcess* is equivalent to linear regression of the basis
-functions with the observations. Since we are taking advantage of the
-sparsity, conditioning the *GaussianProcess* is also just as
-computationally efficient as linear regression.
+(entirely zero) covariance function. We can also construct a
+*GaussianProcess* with a sparse covariance matrix by calling *gpiso*
+and a *SparseRBF* instance. 
 '''
 import numpy as np
 import matplotlib.pyplot as plt
@@ -17,26 +15,28 @@ logging.basicConfig(level=logging.DEBUG)
 np.random.seed(1)
 
 # create synthetic data
-n = 100000
-t = np.linspace(0.0,10.0,n)[:,None]
-s = 0.5*np.ones(n)
-d = 1.0 + 0.2*t[:,0] - 0.05*t[:,0]**2 + np.random.normal(0.0,s)
+n = 10000
+y = np.linspace(-20.0,20.0,n) # observation points
+sigma = 0.5*np.ones(n)
+d = np.exp(-0.3*np.abs(y))*np.sin(y) + np.random.normal(0.0,sigma)
 # evaluate the output at a subset of the observation points
-x = t[::1000]
-
-# create a GP with polynomial basis functions  
-gp = rbf.gauss.gppoly(2)
+x = np.linspace(-20.0,20.0,1000) # interpolation points
+u_true = np.exp(-0.3*np.abs(x))*np.sin(x)  # true signal
+# create a sparse GP
+gp = rbf.gauss.gpiso(rbf.basis.spwen12,(0.0,1.0,4.0)) 
 # condition with the observations
-gpc = gp.condition(t,d,s)
+gpc = gp.condition(y[:,None],d,sigma)
 # find the mean and std of the conditioned GP. Chunk size controls the
 # trade off between speed and memory consumption. It should be tuned
 # by the user.
-u,us = gpc.meansd(x,chunk_size=1000)
+u,us = gpc.meansd(x[:,None],chunk_size=1000)
 
 fig,ax = plt.subplots()
-ax.plot(t[:,0],d,'k.',alpha=0.05,mec='none')
-ax.plot(x[:,0],u,'b-')
-ax.fill_between(x[:,0],u-us,u+us,color='b',alpha=0.2)
-
-
+ax.plot(x,u_true,'k-',label='true signal')
+ax.plot(y,d,'k.',alpha=0.1,mec='none',label='observations')
+ax.plot(x,u,'b-',label='post. mean')
+ax.fill_between(x,u-us,u+us,color='b',alpha=0.2,label='post. std. dev.')
+ax.set_xlim((-20.0,20.0))
+ax.set_ylim((-2.0,2.0))
+ax.legend(loc=2,fontsize=10)
 plt.show()
