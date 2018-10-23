improved error messages when input arrays have the wrong shape

rbf/basis.py
@@ -244,6 +244,7 @@ class RBF(object):
     '''
     x = np.asarray(x, dtype=float)
     assert_shape(x, (None, None), 'x')
+
     c = np.asarray(c, dtype=float)
     assert_shape(c, (None, x.shape[1]), 'c')
 
@@ -253,8 +254,7 @@ class RBF(object):
 
     else:  
       eps = np.asarray(eps, dtype=float)
-
-    assert_shape(eps, (c.shape[0],), 'eps')
+      assert_shape(eps, (c.shape[0],), 'eps')
 
     # if `diff` is not given then take no derivatives
     if diff is None:
@@ -263,8 +263,7 @@ class RBF(object):
     else:
       # make sure diff is immutable
       diff = tuple(diff)
-    
-    assert_shape(diff, (x.shape[1],), 'diff')
+      assert_shape(diff, (x.shape[1],), 'diff')
 
     # add numerical function to cache if not already
     if diff not in self._cache:
@@ -434,6 +433,7 @@ class SparseRBF(RBF):
     '''
     x = np.asarray(x, dtype=float)
     assert_shape(x, (None, None), 'x')
+
     c = np.asarray(c, dtype=float)
     assert_shape(c, (None, x.shape[1]), 'c')
 
@@ -450,8 +450,7 @@ class SparseRBF(RBF):
     else:
       # make sure diff is immutable
       diff = tuple(diff)
-    
-    assert_shape(diff, (x.shape[1],), 'diff')
+      assert_shape(diff, (x.shape[1],), 'diff')
 
     # add numerical function to cache if not already
     if diff not in self._cache:

rbf/fd.py
@@ -7,6 +7,7 @@ import rbf.basis
 import rbf.poly
 import rbf.stencil
 import rbf.linalg
+from rbf.utils import assert_shape
 from rbf.linalg import PartitionedSolver
 import scipy.sparse as sp
 import logging
@@ -19,7 +20,8 @@ def _reshape_diffs(diffs):
   turns diffs into a 2D array
   '''
   if diffs.ndim > 2:
-    raise ValueError('diffs can only be a 1 or 2 dimensional array')
+    raise ValueError('`diffs` can only be a 1 or 2 dimensional array')
+
   D = diffs.shape[-1]
   out = diffs.reshape((-1, D))
   return out
@@ -148,17 +150,21 @@ def weights(x, s, diffs, coeffs=None,
     
   '''
   x = np.asarray(x, dtype=float)
+  assert_shape(x, (None,), 'x')
+  
   s = np.asarray(s, dtype=float)
+  assert_shape(s, (None, x.shape[0]), 's')
+  
   diffs = np.asarray(diffs, dtype=int)
   diffs = _reshape_diffs(diffs)
+  
   # stencil size and number of dimensions
   size, dim = s.shape
   if coeffs is None:
     coeffs = np.ones(diffs.shape[0], dtype=float)
   else:
     coeffs = np.asarray(coeffs, dtype=float)
-    if (coeffs.ndim != 1) | (coeffs.shape[0] != diffs.shape[0]):
-      raise ValueError('`coeffs` and `diffs` have incompatible shapes')
+    assert_shape(coeffs, (diffs.shape[0],), 'coeffs')
 
   max_order = _max_poly_order(size, dim)
   if order is None:
@@ -279,13 +285,20 @@ def weight_matrix(x, p, diffs, coeffs=None,
                          
   '''
   x = np.asarray(x, dtype=float)
+  assert_shape(x, (None, None), 'x')
+  
   p = np.asarray(p, dtype=float)
+  assert_shape(p, (None, x.shape[1]), 'p')
+  
   diffs = np.asarray(diffs, dtype=int)
   diffs = _reshape_diffs(diffs)
+
   if np.isscalar(eps):
     eps = np.full(p.shape[0], eps, dtype=float)
   else:
     eps = np.asarray(eps, dtype=float)  
+    assert_shape(eps, (p.shape[0],), 'eps')
+    
   
   # make `coeffs` a (K, N) array
   if coeffs is None:
@@ -294,6 +307,8 @@ def weight_matrix(x, p, diffs, coeffs=None,
     coeffs = np.asarray(coeffs, dtype=float)
     if coeffs.ndim == 1:
       coeffs = np.repeat(coeffs[:, None], x.shape[0], axis=1) 
+
+    assert_shape(coeffs, (diffs.shape[0], x.shape[0]), 'coeffs')      
    
   if stencils is None:
     if n is None:
@@ -303,8 +318,10 @@ def weight_matrix(x, p, diffs, coeffs=None,
       n = min(n, p.shape[0])
 
     stencils = rbf.stencil.stencil_network(x, p, n)
-
-  stencils = np.asarray(stencils, dtype=int)
+  else:    
+    stencils = np.asarray(stencils, dtype=int)
+    assert_shape(stencils, (x.shape[0], None), 'stencils')
+  
   logger.debug(
     'building a (%s, %s) RBF-FD weight matrix with %s nonzeros...' 
     % (x.shape[0], p.shape[0], stencils.size))   
@@ -351,11 +368,15 @@ def add_rows(A, B, idx):
   (n1, m) csc sparse matrix
   
   '''
-  idx = np.asarray(idx, dtype=int)
   # coerce `A` to csc to enforce a consistent output type
   A = sp.csc_matrix(A)
   # convert `B` to a coo matrix, and expand out its rows
   B = sp.coo_matrix(B)
+  assert_shape(B, (None, A.shape[1]), 'B')
+    
+  idx = np.asarray(idx, dtype=int)
+  assert_shape(idx, (B.shape[0],), 'idx')
+
   B = sp.csc_matrix((B.data, (idx[B.row], B.col)), shape=A.shape)
   # Now add the expanded `B` to `A`, 
   out = A + B

rbf/interpolate.py
@@ -55,8 +55,9 @@ import scipy.sparse
 import scipy.optimize
 import scipy.spatial
 import rbf.basis
-import rbf.poly
 import rbf.geometry
+import rbf.poly 
+from rbf.utils import assert_shape
 from rbf.linalg import PartitionedSolver
 
 
@@ -68,7 +69,7 @@ def _in_hull(p, hull):
   # if there are not enough points in `hull` to form a simplex then 
   # return False for each point in `p`.
   if hull.shape[0] <= dim:
-    return np.zeros(p.shape[0],dtype=bool)
+    return np.zeros(p.shape[0], dtype=bool)
   
   if dim >= 2:
     hull = scipy.spatial.Delaunay(hull)
@@ -77,7 +78,7 @@ def _in_hull(p, hull):
     # one dimensional points
     min = np.min(hull)
     max = np.max(hull)
-    return (p[:,0] >= min) & (p[:,0] <= max)
+    return (p[:, 0] >= min) & (p[:, 0] <= max)
 
 
 class RBFInterpolant(object):
@@ -86,7 +87,7 @@ class RBFInterpolant(object):
 
   Parameters 
   ---------- 
-  y : (N,D) array
+  y : (N, D) array
     Observation points.
 
   d : (N,) array
@@ -135,11 +136,18 @@ class RBFInterpolant(object):
   [2] Schimek, M., Smoothing and Regression: Approaches, Computations, 
   and Applications. John Wiley & Sons, 2000.
   '''
-  def __init__(self,y,d,sigma=None,eps=1.0,basis=rbf.basis.phs3,
-               order=1,extrapolate=True):
-
+  def __init__(self, y, d,
+               sigma=None,
+               eps=1.0,
+               basis=rbf.basis.phs3,
+               order=1,
+               extrapolate=True):
     y = np.asarray(y) 
+    assert_shape(y, (None, None), 'y')
+    
     d = np.asarray(d)
+    assert_shape(d, (y.shape[0],), 'd')
+    
     q,dim = y.shape
 
     if sigma is None:
@@ -153,18 +161,19 @@ class RBFInterpolant(object):
 
     else:
       sigma = np.asarray(sigma)
+      assert_shape(sigma, (y.shape[0],), 'sigma')
       
     # form block consisting of the RBF and uncertainties on the
     # diagonal
-    K = basis(y,y,eps=eps) 
+    K = basis(y, y, eps=eps) 
     Cd = scipy.sparse.diags(sigma**2)
-    # for the block consisting of the monomials
-    powers = rbf.poly.powers(order,dim)
-    P = rbf.poly.mvmonos(y,powers)
+    # form the block consisting of the monomials
+    powers = rbf.poly.powers(order, dim)
+    P = rbf.poly.mvmonos(y, powers)
     # create zeros vector for the right-hand-side
     z = np.zeros((powers.shape[0],))
     # solve for the RBF and mononomial coefficients
-    basis_coeff,poly_coeff = PartitionedSolver(K + Cd,P).solve(d,z) 
+    basis_coeff, poly_coeff = PartitionedSolver(K + Cd, P).solve(d, z) 
 
     self._y = y
     self._basis = basis
@@ -175,21 +184,21 @@ class RBFInterpolant(object):
     self._powers = powers 
     self.extrapolate = extrapolate
 
-  def __call__(self,x,diff=None,chunk_size=1000):
+  def __call__(self, x, diff=None, chunk_size=1000):
     ''' 
     Evaluates the interpolant at `x`
 
     Parameters 
     ---------- 
-    x : (N,D) array
+    x : (N, D) array
       Target points.
 
     diff : (D,) int array, optional
       Derivative order for each spatial dimension.
         
     chunk_size : int, optional  
-      Break `x` into chunks with this size and evaluate the 
-      interpolant for each chunk.  Smaller values result in decreased 
+      Break `x` into chunks with this size and evaluate the
+      interpolant for each chunk.  Smaller values result in decreased
       memory usage but also decreased speed.
 
     Returns
@@ -199,14 +208,23 @@ class RBFInterpolant(object):
       
     '''
     x = np.asarray(x,dtype=float) 
+    assert_shape(x, (None, self._y.shape[1]), 'x')
+    
     xlen = x.shape[0]
     # allocate output array
-    out = np.zeros(xlen,dtype=float)
+    out = np.zeros(xlen, dtype=float)
     count = 0
     while count < xlen:
-      start,stop = count,count+chunk_size
-      K = self._basis(x[start:stop],self._y,eps=self._eps,diff=diff)
-      P = rbf.poly.mvmonos(x[start:stop],self._powers,diff=diff)
+      start, stop = count, count + chunk_size
+      K = self._basis(
+        x[start:stop], 
+        self._y, 
+        eps=self._eps, 
+        diff=diff)
+      P = rbf.poly.mvmonos(
+        x[start:stop], 
+        self._powers, 
+        diff=diff)
       out[start:stop] = (K.dot(self._basis_coeff) + 
                          P.dot(self._poly_coeff))
       count += chunk_size
@@ -214,7 +232,7 @@ class RBFInterpolant(object):
     # return zero for points outside of the convex hull if 
     # extrapolation is not allowed
     if not self.extrapolate:
-      out[~_in_hull(x,self._y)] = np.nan
+      out[~_in_hull(x, self._y)] = np.nan
 
     return out
 

rbf/nodes.py
@@ -9,6 +9,7 @@ import logging
 from scipy.sparse import csc_matrix
 from scipy.sparse.csgraph import reverse_cuthill_mckee
 from rbf.stencil import stencil_network
+from rbf.utils import assert_shape
 from rbf.geometry import (intersection_count,
                           intersection_index,
                           intersection_point,
@@ -484,17 +485,17 @@ def min_energy_nodes(N, vert, smp,
   Parameters
   ----------
   N : int
-    Number of nodes.
+    Number of nodes
 
   vert : (P, D) array
-    Vertices making up the boundary.
+    Vertices making up the boundary
 
   smp : (Q, D) array
-    Describes how the vertices are connected to form the boundary.
+    Describes how the vertices are connected to form the boundary
 
   rho : function, optional
-    Node density function. Takes a (*, D) array of coordinates in D
-    dimensional space and returns an (*,) array of densities which
+    Node density function. Takes a (?, D) array of coordinates in D
+    dimensional space and returns an (?,) array of densities which
     have been normalized so that the maximum density in the domain is
     1.0. This function will still work if the maximum value is
     normalized to something less than 1.0; however it will be less
@@ -542,14 +543,14 @@ def min_energy_nodes(N, vert, smp,
   include_vertices : bool, optional
     If `True`, then the vertices will be included in the output nodes.
     Each vertex will be assigned to the boundary group that its
-    ajoining simplices are part of. If the simplices are in multiple
+    adjoining simplices are part of. If the simplices are in multiple
     groups, then the vertex will be assigned to the group containing
     the simplex that comes first in `smp`.
 
   Returns
   -------
   (N, D) float array
-    Nodes positions.
+    Nodes positions
 
   dict 
     The indices of nodes belonging to each group. There will always be
@@ -566,7 +567,6 @@ def min_energy_nodes(N, vert, smp,
   (N, D) float array
     Outward normal vectors for each node. If a node is not on the
     boundary then its corresponding row will contain NaNs.
-    then the corresponding row will contain NaN's.
 
   Notes
   -----
@@ -623,7 +623,11 @@ def min_energy_nodes(N, vert, smp,
   '''
   logger.debug('starting minimum energy node generation')
   vert = np.asarray(vert, dtype=float)
+  assert_shape(vert, (None, None), 'vert')
+  
   smp = np.asarray(smp, dtype=int)
+  assert_shape(smp, (None, vert.shape[1]), 'smp')
+  
   if boundary_groups is None:
     boundary_groups = {'all': range(smp.shape[0])}
     for i in range(smp.shape[0]):
@@ -631,6 +635,8 @@ def min_energy_nodes(N, vert, smp,
     
   if pinned_nodes is None:
     pinned_nodes = np.zeros((0, vert.shape[1]), dtype=float)
+
+  assert_shape(pinned_nodes, (None, vert.shape[1]), 'pinned_nodes')
   
   logger.debug('finding node positions with rejection sampling')
   nodes = _rejection_sampling_nodes(N, vert, smp, rho=rho)

rbf/stencil.py
@@ -6,6 +6,7 @@ from __future__ import division
 import numpy as np
 from scipy.spatial import cKDTree
 from rbf.geometry import intersection_count
+from rbf.utils import assert_shape
 import networkx
 
 class StencilError(Exception):
@@ -20,19 +21,19 @@ def stencils_to_edges(stencils):
   
   Parameters
   ----------
-  stencil : (N,D) int aray
+  stencil : (N, D) int aray
 
   Returns
   -------
-  edges : (K,2) int array
+  edges : (K, 2) int array
 
   '''
   stencils = np.asarray(stencils)
-  N,S = stencils.shape
-  node1 = np.arange(N)[:,None].repeat(S,axis=1)
-  node2 = np.array(stencils,copy=True)
-  edges = zip(node1.flatten(),node2.flatten())
-  edges = np.array(edges,dtype=int)
+  N, S = stencils.shape
+  node1 = np.arange(N)[:, None].repeat(S, axis=1)
+  node2 = np.array(stencils, copy=True)
+  edges = zip(node1.flatten(), node2.flatten())
+  edges = np.array(edges, dtype=int)
   return edges
 
 
@@ -43,7 +44,7 @@ def is_connected(stencils):
 
   Parameters
   ----------
-  stencil : (N,D) int aray
+  stencil : (N, D) int aray
 
   Returns
   -------
@@ -64,7 +65,7 @@ def connectivity(stencils):
 
   Parameters
   ----------
-  stencil : (N,D) int aray
+  stencil : (N, D) int aray
 
   Returns
   -------
@@ -78,67 +79,67 @@ def connectivity(stencils):
   return networkx.node_connectivity(graph)
 
 
-def _closest_argsort(c,x):
+def _closest_argsort(c, x):
   ''' 
   Returns the indices of nodes in `x` sorted in order of distance to `c`
   '''
-  dist = np.sum((x - c[None,:])**2,axis=1)
+  dist = np.sum((x - c[None, :])**2, axis=1)
   idx = np.argsort(dist)
   return idx
 
 
-def _has_intersections(c,x,vert,smp):
+def _has_intersections(c, x, vert, smp):
   ''' 
-  Check if any of the edges (`c`,`x[i]`) intersect the boundary 
+  Check if any of the edges (`c`, `x[i]`) intersect the boundary 
   defined by `vert` and `smp`. 
   '''
   N = len(x)
-  cext = np.repeat(c[None,:],N,axis=0)
+  cext = np.repeat(c[None, :], N, axis=0)
   # number of times each edge intersects the boundary
-  count = intersection_count(x,cext,vert,smp)
+  count = intersection_count(x, cext, vert, smp)
   # return True if there are intersections
   out = np.any(count > 0)
   return out
 
 
-def _stencil(c,x,n,vert,smp):
+def _stencil(c, x, n, vert, smp):
   ''' 
   Forms a stencil about `c` made up of `n` nearby nodes in `x`. The 
   stencil is constrained so that it does not reach across the boundary 
   defined by `vert` and `smp`.
   '''
-  sorted_idx = _closest_argsort(c,x)
+  sorted_idx = _closest_argsort(c, x)
   stencil_idx = []    
   for si in sorted_idx:
     if len(stencil_idx) == n:
       break
 
     stencil_idx += [si]
-    if _has_intersections(c,x[stencil_idx],vert,smp):
+    if _has_intersections(c, x[stencil_idx], vert, smp):
       stencil_idx.pop()
 
   if len(stencil_idx) == n:
-    return np.array(stencil_idx,dtype=int)
+    return np.array(stencil_idx, dtype=int)
   else: 
     raise StencilError('cannot not form a stencil with size %s' % n)
 
 
-def _stencil_network_no_boundary(x,p,n):
+def _stencil_network_no_boundary(x, p, n):
   ''' 
   Returns the `n` nearest points in `p` for each point in `x`
   '''
   if n == 0:
-    out = np.zeros((x.shape[0],0),dtype=int)
+    out = np.zeros((x.shape[0], 0), dtype=int)
   else:
     T = cKDTree(p)
-    dummy,out = T.query(x,n)
+    dummy, out = T.query(x, n)
     if n == 1:
-      out = out[:,None]
+      out = out[:, None]
 
   return out 
 
 
-def stencil_network(x,p,n,vert=None,smp=None):
+def stencil_network(x, p, n, vert=None, smp=None):
   ''' 
   Forms a stencil for each point in `x`. Each stencil is made up of 
   `n` nearby points from `p`. Stencils can be constrained to not 
@@ -146,51 +147,58 @@ def stencil_network(x,p,n,vert=None,smp=None):
   
   Parameters
   ----------
-  x : (N,D) array
+  x : (N, D) array
     Target points. A stencil will be made for each point in `x`.
 
-  p : (M,D) array
+  p : (M, D) array
     Source points. The stencils will be made up of points from `p`.
 
   n : int
     Stencil size.
 
-  vert : (P,D) array, optional
+  vert : (P, D) array, optional
     Vertices of the boundary which stencils cannot intersect.
 
-  smp : (Q,D) array, optional
+  smp : (Q, D) array, optional
     Connectivity of the vertices to form the boundary.
 
   Returns
   -------
-  sn : (N,D) array
+  sn : (N, D) array
     Indices of points in `p` which form a stencil for each point in 
     `x`.
     
   '''
-  x = np.asarray(x,dtype=float)
-  p = np.asarray(p,dtype=float)
+  x = np.asarray(x, dtype=float)
+  assert_shape(x, (None, None), 'x')
+  
+  p = np.asarray(p, dtype=float)
+  assert_shape(p, (None, x.shape[1]), 'p')
+  
   Nx = x.shape[0]
   Np = p.shape[0]
   if n > Np:
     raise StencilError(
-      'cannot form a stencil with size %s from %s nodes' % (n,Np))
+      'cannot form a stencil with size %s from %s nodes' % (n, Np))
     
   if (vert is None) | (smp is None):
-    vert = np.zeros((0,x.shape[1]),dtype=float)
-    smp = np.zeros((0,x.shape[1]),dtype=int)
+    vert = np.zeros((0, x.shape[1]), dtype=float)
+    smp = np.zeros((0, x.shape[1]), dtype=int)
   
   else:
-    vert = np.asarray(vert,dtype=float)
-    smp = np.asarray(smp,dtype=int)
+    vert = np.asarray(vert, dtype=float)
+    assert_shape(vert, (None, x.shape[1]), 'vert')
     
-  sn = _stencil_network_no_boundary(x,p,n)
+    smp = np.asarray(smp, dtype=int)
+    assert_shape(smp, (None, x.shape[1]), 'smp')
+    
+  sn = _stencil_network_no_boundary(x, p, n)
   if smp.shape[0] == 0:
     return sn
 
   # ensure that no stencils intersect the boundary
   for i in range(Nx):
-    if _has_intersections(x[i],p[sn[i]],vert,smp):
-      sn[i,:] = _stencil(x[i],p,n,vert,smp)
+    if _has_intersections(x[i], p[sn[i]], vert, smp):
+      sn[i, :] = _stencil(x[i], p, n, vert, smp)
 
   return sn

rbf/utils.py
@@ -6,25 +6,29 @@ from collections import OrderedDict
 import numpy as np
 
 
-def assert_shape(a,shape,label):
+def assert_shape(arr, shape, label):
   ''' 
-  Raises an error if *a* does not have the specified shape. If an 
-  element in *shape* is *None* then that axis can have any length.
+  Raises an error if `arr` does not have the specified shape. If an
+  element in `shape` is `None` then that axis can have any length.
   '''
-  ashape = np.shape(a)
-  if len(ashape) != len(shape):
+  if hasattr(arr, 'shape'):
+    arr_shape = arr.shape
+  else:
+    arr_shape = np.shape(arr)
+    
+  if len(arr_shape) != len(shape):
     raise ValueError(
-      '*%s* is a %s dimensional array but it should be a %s '
-      'dimensional array' % (label, len(ashape), len(shape)))
+      '`%s` is a %s dimensional array but it should be a %s '
+      'dimensional array' % (label, len(arr_shape), len(shape)))
 
-  for axis,(i,j) in enumerate(zip(ashape,shape)):
+  for axis, (i, j) in enumerate(zip(arr_shape, shape)):
     if j is None:
       continue
 
     if i != j:
       raise ValueError(
-        'axis %s of *%s* has length %s but it should have length %s.' %
-        (axis,label,i,j))
+        'Axis %s of `%s` has length %s but it should have length '
+        '%s.' % (axis, label, i, j))
 
   return
 
@@ -36,7 +40,7 @@ def get_arg_count(func):
   '''
   # get the python version. If < 3.3 then use inspect.getargspec,
   # otherwise use inspect.signature
-  if sys.version_info < (3,3):
+  if sys.version_info < (3, 3):
     argspec = inspect.getargspec(func)
     # if the function has variable positional arguments then return -1
     if argspec.varargs is not None:
@@ -54,9 +58,9 @@ def get_arg_count(func):
     if any(p.kind == 2 for p in params.values()):
       return -1
 
-    # if a parameter has kind 0 then it is a a positional only argument
-    # and if kind is 1 then it is a positional or keyword argument.
-    # Count the 0's and 1's
+    # if a parameter has kind 0 then it is a a positional only
+    # argument and if kind is 1 then it is a positional or keyword
+    # argument. Count the 0's and 1's
     out = sum((p.kind == 0) | (p.kind == 1) for p in params.values())
     return out
 
@@ -71,12 +75,12 @@ class Memoize(object):
   Parameters
   ----------
   fin : function
-    Function that takes arrays as input.
+    Function that takes arrays as input
   
   Returns
   -------
   fout : function
-    Memoized function.
+    Memoized function
   
   Notes
   -----
@@ -84,7 +88,7 @@ class Memoize(object):
   `clear_caches`.
       
   '''
-  # variable controlling the maximum cache size for all memoized 
+  # variable controlling the maximum cache size for all memoized
   # functions
   MAX_CACHE_SIZE = 100
   # collection of weak references to all instances