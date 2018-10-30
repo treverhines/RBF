fixed a bug in the memoizer for GaussianProcesses. Made the memoizer LRU. Added function to basis.py that changes the way sympy expressions are converted to numerical and then clears all RBF caches

rbf/basis.py
@@ -39,18 +39,34 @@ Wendland (d=3, k=2)                wen32         Yes (1, 2, and 3-D)  :math:`(1 
 ''' 
 from __future__ import division 
 import logging
+import weakref
 
 import sympy 
 import numpy as np 
 from scipy.sparse import csc_matrix
 from scipy.spatial import cKDTree
 from sympy.utilities.autowrap import ufuncify
+from sympy import lambdify
 
 from rbf.poly import powers
 from rbf.utils import assert_shape
 logger = logging.getLogger(__name__)
 
 
+# the method used to convert sympy expressions to numerical functions
+_NUMERICAL_CONVERTER = 'ufuncify'
+
+
+def set_numerical_converter(value):
+    global _NUMERICAL_CONVERTER
+    if value not in {'lambdify', 'ufuncify'}:
+        raise ValueError(
+            '`value` must be either "lambdify" or "ufuncify"')
+            
+    _NUMERICAL_CONVERTER = value
+    clear_rbf_caches()
+
+    
 def get_r():
   ''' 
   returns the symbolic variable for :math:`r` which is used to 
@@ -149,6 +165,8 @@ class RBF(object):
   It is safe to change the attributes `tol` and `limits`. Changes will
   cause the cache of numerical functions to be cleared.
   '''
+  _INSTANCES = []
+  
   @property
   def expr(self):
     # `expr` is read-only. 
@@ -213,6 +231,8 @@ class RBF(object):
     
     ## create the cache for numerical functions    
     self._cache = {}
+
+    RBF._INSTANCES += [weakref.ref(self)]
 
   def __call__(self, x, c, eps=1.0, diff=None):
     ''' 
@@ -331,8 +351,14 @@ class RBF(object):
       # create a piecewise symbolic function which is `lim` when
       # `r_sym < tol` and `expr` otherwise
       expr = sympy.Piecewise((lim, r_sym < self.tol), (expr, True)) 
-      
-    func = ufuncify(x_sym + c_sym + (_EPS,), expr, backend='numpy')
+
+    if _NUMERICAL_CONVERTER == 'ufuncify':      
+      func = ufuncify(x_sym + c_sym + (_EPS,), expr, backend='numpy')
+    elif _NUMERICAL_CONVERTER == 'lambdify':
+      func = lambdify(x_sym + c_sym + (_EPS,), expr, modules=['numpy'])
+    else:
+      raise ValueError()          
+        
     self._cache[diff] = func
     logger.debug('The numerical function has been created and cached')
     
@@ -389,7 +415,6 @@ class SparseRBF(RBF):
   @property
   def supp(self):
     return self._supp
-    
   
   def __init__(self, expr, supp, **kwargs):
     RBF.__init__(self, expr, **kwargs)
@@ -497,6 +522,15 @@ class SparseRBF(RBF):
     return out
 
   
+def clear_rbf_caches():
+    '''
+    Clear the caches of numerical functions for all the RBF instances
+    '''
+    for inst in RBF._INSTANCES:
+        if inst() is not None:
+            inst().clear_cache()
+
+
 ## Instantiate some common RBFs
 #####################################################################
 _phs8_limits = {}

rbf/fd.py
@@ -2,15 +2,18 @@
 This module provides functions for generating RBF-FD weights
 '''
 from __future__ import division
+from functools import lru_cache
+import logging
+
 import numpy as np
+import scipy.sparse as sp
+
 import rbf.basis
 import rbf.poly
 import rbf.stencil
 import rbf.linalg
 from rbf.utils import assert_shape
 from rbf.linalg import PartitionedSolver
-import scipy.sparse as sp
-import logging
 
 logger = logging.getLogger(__name__)
 
@@ -49,6 +52,7 @@ def _default_poly_order(diffs):
   return P
 
 
+@lru_cache(maxsize=128, typed=True)
 def _max_poly_order(size, dim):
   ''' 
   Returns the maximum polynomial order allowed for the given stencil 

rbf/gauss.py
@@ -377,7 +377,7 @@ import scipy.sparse as sp
 import rbf.poly
 import rbf.basis
 import rbf.linalg
-from rbf.utils import assert_shape, get_arg_count, Memoize
+from rbf.utils import assert_shape, get_arg_count, MemoizeArrayInput
 from rbf.linalg import (as_array, as_sparse_or_array, 
                         is_positive_definite, PosDefSolver, 
                         PartitionedPosDefSolver)
@@ -649,7 +649,7 @@ def _condition(gp, y, d, sigma, p, obs_diff):
   '''   
   Returns a conditioned `GaussianProcess`.
   '''
-  @Memoize
+  @MemoizeArrayInput
   def precompute():
     # do as many calculations as possible without yet knowning where
     # the interpolation points will be. This function is memoized so
@@ -1720,9 +1720,9 @@ class GaussianProcess(object):
     redundant computations, but it may also increase memory
     consumption.
     '''
-    self._mean = Memoize(self._mean)
-    self._covariance = Memoize(self._covariance)
-    self._basis = Memoize(self._basis)
+    self._mean = MemoizeArrayInput(self._mean)
+    self._covariance = MemoizeArrayInput(self._covariance)
+    self._basis = MemoizeArrayInput(self._basis)
 
 
 def gpiso(phi, params, dim=None):