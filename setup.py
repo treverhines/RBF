48ba367262af58e0de6af1c21e94b943a2a3e515
Trever Hines <treverhines@gmail.com>
2019-11-16 13:17:19 -0500

    removed np.get_include from setup because i no longer cimport numpy

--- setup.py
+++ setup.py
@@ -3,24 +3,18 @@ if __name__ == '__main__':
   from setuptools import setup
   from setuptools.extension import Extension
   from Cython.Build import cythonize
-  import numpy as np
   import versioneer
   ext = []
   ext += [Extension(name='rbf.poly',
-                    sources=['rbf/poly.pyx'],
-                    include_dirs=[np.get_include()])]
+                    sources=['rbf/poly.pyx'])]
   ext += [Extension(name='rbf.sputils',
-                    sources=['rbf/sputils.pyx'],
-                    include_dirs=[np.get_include()])]
+                    sources=['rbf/sputils.pyx'])]
   ext += [Extension(name='rbf.pde.halton',
-                    sources=['rbf/pde/halton.pyx'],
-                    include_dirs=[np.get_include()])]
+                    sources=['rbf/pde/halton.pyx'])]
   ext += [Extension(name='rbf.pde.geometry',
-                    sources=['rbf/pde/geometry.pyx'],
-                    include_dirs=[np.get_include()])]
+                    sources=['rbf/pde/geometry.pyx'])]
   ext += [Extension(name='rbf.pde.sampling',
-                    sources=['rbf/pde/sampling.pyx'],
-                    include_dirs=[np.get_include()])]
+                    sources=['rbf/pde/sampling.pyx'])]
   setup(name='RBF',
         version=versioneer.get_version(),
         cmdclass=versioneer.get_cmdclass(),
