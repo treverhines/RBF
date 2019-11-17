b35274a5e4f289435efcb1bbf847214fc6b1cb60
Trever Hines <treverhines@gmail.com>
2019-11-17 16:41:10 -0500

    added test to conda.recipe and added doc url

---
+++ conda.recipe/run_test.py
@@ -0,0 +1,16 @@
+import rbf
+import rbf.utils
+import rbf.sputils
+import rbf.basis
+import rbf.linalg
+import rbf.poly
+import rbf.interpolate
+import rbf.gauss
+import rbf.pde.geometry
+import rbf.pde.halton
+import rbf.pde.sampling
+import rbf.pde.fd
+import rbf.pde.nodes
+import rbf.pde.domain
+import rbf.pde.elastic
+print('version: %s' % rbf.__version__)
