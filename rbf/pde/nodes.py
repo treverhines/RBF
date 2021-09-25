0840d40019b0168150ce4aec85c58bfe10e66d8b
Trever Hines <treverhines@gmail.com>
2021-09-25 10:09:15 -0400

    refactored nodes.py:
    - added iterations argument to `disperse`.
    - Suppress some division by zero warnings that get handled in `disperse`.
    - The value specified for `neighbors` is now actually the number of neighbors
    used for dispersion (not including the node itself).
    - Added `ghost_delta` argument to `prepare_nodes` to control how far off the
    boundary to place ghost nodes.
    - Added validation for `boundary_groups`.
    - Optimized how nodes are assigned to groups.
    - And other formatting changes.

--- rbf/pde/nodes.py
+++ rbf/pde/nodes.py
@@ -3,6 +3,8 @@ This module provides functions for generating nodes used for solving PDEs with
 the RBF and RBF-FD method.
 '''
 from __future__ import division
+from itertools import chain
+from collections import Counter
 import logging
 
 import numpy as np
@@ -17,46 +19,55 @@ from rbf.pde.sampling import rejection_sampling, poisson_discs
 logger = logging.getLogger(__name__)
 
 
-def _disperse(nodes, rho, fixed_nodes, neighbors, delta):
+def _disperse_step(nodes, rho, fixed_nodes, neighbors, delta):
     '''
     Returns the new position of the free nodes after a dispersal step. This
     does not handle node intersections with the boundary.
     '''
-    # form collection of all nodes
+    if nodes.shape[0] == 0:
+        # If there are no nodes, avoid errors resulting from zero sized arrays.
+        return nodes.copy()
+
     all_nodes = np.vstack((nodes, fixed_nodes))
     # find index and distance to nearest nodes
-    d, i = KDTree(all_nodes).query(nodes, neighbors)
+    dist, idx = KDTree(all_nodes).query(nodes, neighbors + 1)
     # dont consider a node to be one of its own nearest neighbors
-    d, i = d[:, 1:], i[:, 1:]
+    dist, idx = dist[:, 1:], idx[:, 1:]
     # compute the force proportionality constant between each node
     # based on their charges
-    c = 1.0/(rho(all_nodes)[i, None]*rho(nodes)[:, None, None])
-    # calculate forces on each node resulting from the `m` nearest nodes.
-    forces = c*(nodes[:, None, :] - all_nodes[i, :])/d[:, :, None]**3
-    # sum up all the forces for each node
+    c = 1.0/(rho(all_nodes)[idx, None]*rho(nodes)[:, None, None])
+    # calculate forces on each node resulting from the neighboring nodes. This
+    # will result in a division by zero warning if there are duplicate nodes.
+    # Do not suppress the warning because it is a real problem.
+    forces = c*(nodes[:, None, :] - all_nodes[idx, :])/dist[:, :, None]**3
+    # sum up all the forces for each node to get the direction that the nodes
+    # should move.
     direction = np.sum(forces, axis=1)
-    # normalize the net forces to one
-    direction /= np.linalg.norm(direction, axis=1)[:, None]
-    # in the case of a zero vector replace nans with zeros
-    direction = np.nan_to_num(direction)
-    # move in the direction of the force by an amount proportional to the
-    # distance to the nearest neighbor
-    step = delta*d[:, 0, None]*direction
+    # normalize the direction to one. It is possible that the net force is
+    # exactly zero. In that case, the node should not move.
+    with np.errstate(invalid='ignore'):
+        direction /= np.linalg.norm(direction, axis=1)[:, None]
+        direction = np.nan_to_num(direction)
+
+    # move by an amount proportional to the distance to the nearest neighbor.
+    step = delta*dist[:, 0, None]*direction
     # new node positions
     out = nodes + step
     return out
 
 
 def disperse(nodes, domain,
+             iterations=20,
              rho=None,
              fixed_nodes=None,
              neighbors=None,
              delta=0.1):
     '''
-    Slightly disperses the nodes within the domain. The disperson is analogous
-    to electrostatic repulsion, where neighboring node exert a repulsive force
-    on eachother. If a node is repelled into a boundary then it bounces back
-    in.
+    Disperses the nodes within the domain. The dispersion is analogous to
+    electrostatic repulsion, where neighboring nodes exert a repulsive force on
+    eachother. Each node steps in the direction of its net repulsive force with
+    a step size proportional to the distance to its nearest neighbor. If a node
+    is repelled into a boundary then it bounces back in.
 
     Parameters
     ----------
@@ -66,16 +77,19 @@ def disperse(nodes, domain,
     domain : (p, d) float array and (q, d) int array
         Vertices of the domain and connectivity of the vertices.
 
+    iterations : int, optional
+        Number of dispersion iterations.
+
     rho : callable, optional
         Takes an (n, d) array as input and returns the repulsion force for a
         node at those position.
 
     fixed_nodes : (k, d) float array, optional
-        Nodes which do not move and only provide a repulsion force
+        Nodes which do not move and only provide a repulsion force.
 
     neighbors : int, optional
         The number of adjacent nodes used to determine repulsion forces for
-        each node
+        each node.
 
     delta : float, optional
         The step size. Each node moves in the direction of the repulsion force
@@ -102,47 +116,60 @@ def disperse(nodes, domain,
 
     if neighbors is None:
         # the default number of neighboring nodes to use when computing the
-        # repulsion force is 4 for 2D and 5 for 3D
+        # repulsion force is 3 for 2D and 4 for 3D
         if domain.dim == 2:
-            neighbors = 4
+            neighbors = 3
 
         elif domain.dim == 3:
-            neighbors = 5
+            neighbors = 4
 
     # ensure that the number of neighboring nodes used for the repulsion force
     # is less than or equal to the total number of nodes
-    neighbors = min(neighbors, nodes.shape[0] + fixed_nodes.shape[0])
-    # if m is 0 or 1 then the nodes remain stationary
-    if neighbors <= 1:
-        return np.array(nodes, copy=True)
+    neighbors = min(neighbors, nodes.shape[0] + fixed_nodes.shape[0] - 1)
 
-    # node positions after repulsion
-    out = _disperse(nodes, rho, fixed_nodes, neighbors, delta)
-    # indices of nodes which are now outside the domain
-    crossed = domain.intersection_count(nodes, out) > 0
-    crossed, = crossed.nonzero()
-    # points where nodes intersected the boundary and the simplex they
-    # intersected at
-    intr_pnt, intr_idx = domain.intersection_point(
-        nodes[crossed], out[crossed])
+    for itr in range(iterations):
+        logger.debug(
+            'Starting node dispersion iterations %s of %s.'
+            % (itr + 1, iterations)
+            )
 
-    # normal vector to intersection points
-    intr_norms = domain.normals[intr_idx]
-    # distance that the node wanted to travel beyond the boundary
-    res = out[crossed] - intr_pnt
-    # bounce node off the boundary
-    out[crossed] -= 2*intr_norms*np.sum(res*intr_norms, 1)[:, None]
-    # check to see if the bounced nodes still intersect the boundary. If they
-    # do, then set them back to their original position
-    still_crossed = domain.intersection_count(nodes[crossed], out[crossed]) > 0
-    out[crossed[still_crossed]] = nodes[crossed[still_crossed]]
-    return out
+        new_nodes = _disperse_step(nodes, rho, fixed_nodes, neighbors, delta)
+        # If the line segment connecting the new and old node crosses the
+        # boundary, then the node should bounce off the boundary.
+        crossed, = domain.intersection_count(nodes, new_nodes).nonzero()
+        # points where nodes intersected the boundary and the simplex they
+        # intersected at
+        intr_pnt, intr_idx = domain.intersection_point(
+            nodes[crossed],
+            new_nodes[crossed]
+            )
+
+        # normal vector to the intersection points
+        intr_norms = domain.normals[intr_idx]
+        # residual distance that the nodes wanted to travel beyond the boundary
+        res = new_nodes[crossed] - intr_pnt
+        # normal component of the residuals
+        res_perp = np.sum(res*intr_norms, axis=1)
+        # bounce nodes off the boundary
+        new_nodes[crossed] -= 2*intr_norms*res_perp[:, None]
+        # check to see if the bounced nodes are still crossing the boundary. If
+        # they are, then set them back to their original position. Do not
+        # bother with multiple bounces.
+        still_crossed, = domain.intersection_count(
+            nodes[crossed],
+            new_nodes[crossed]
+            ).nonzero()
+
+        new_nodes[crossed[still_crossed]] = nodes[crossed[still_crossed]]
+        nodes = new_nodes
+
+    return nodes
 
 
 def neighbor_argsort(nodes, m=None):
     '''
     Returns a permutation array that sorts `nodes` so that each node and its
-    `m` nearest neighbors are close together in memory. This is done through
+    `m-1` nearest neighbors are close together in memory. This is done through
     the use of a KD Tree and the Reverse Cuthill-McKee algorithm.
 
     Parameters
@@ -200,13 +227,14 @@ def _check_spacing(nodes, rho=None):
 
     # distance to nearest neighbor
     dist = KDTree(nodes).query(nodes, 2)[0][:, 1]
-    if np.any(dist == 0.0):
-        is_zero = (dist == 0.0)
-        indices, = is_zero.nonzero()
+    dist_is_zero = (dist == 0.0)
+    if np.any(dist_is_zero):
+        indices, = dist_is_zero.nonzero()
         for idx in indices:
             logger.warning(
                 'Node %s (%s) is in the same location as another node.'
-                % (idx, nodes[idx]))
+                % (idx, nodes[idx])
+                )
 
     density = 1.0/dist**dim
     normalized_density = np.log10(density / rho(nodes))
@@ -219,7 +247,8 @@ def _check_spacing(nodes, rho=None):
         for idx in indices:
             logger.warning(
                 'Node %s (%s) is unusually close to a neighboring node.'
-                % (idx, nodes[idx]))
+                % (idx, nodes[idx])
+                )
 
 
 def prepare_nodes(nodes, domain,
@@ -231,6 +260,7 @@ def prepare_nodes(nodes, domain,
                   snap_delta=0.5,
                   boundary_groups=None,
                   boundary_groups_with_ghosts=None,
+                  ghost_delta=0.5,
                   include_vertices=False,
                   orient_simplices=True):
     '''
@@ -262,7 +292,7 @@ def prepare_nodes(nodes, domain,
 
     neighbors : int, optional
         Number of neighboring nodes to use when calculating the repulsion
-        force. This defaults to 4 for 2D nodes and 5 for 3D nodes.
+        force. This defaults to 3 for 2D nodes and 4 for 3D nodes.
 
     dispersion_delta : float, optional
         Scaling factor for the node step size in each iteration. The step size
@@ -291,6 +321,11 @@ def prepare_nodes(nodes, domain,
         List of boundary groups that will be given ghost nodes. By default, no
         boundary groups are given ghost nodes. The groups specified here must
         exist in `boundary_groups`.
+
+    ghost_delta : float, optional
+        How far the ghost nodes should be from their corresponding boundary
+        node. The distance is `ghost_delta` times the distance to the nearest
+        neighbor.
 
     include_vertices : bool, optional
         If `True`, then the vertices will be included in the output nodes. Each
@@ -324,13 +359,16 @@ def prepare_nodes(nodes, domain,
 
     '''
     domain = as_domain(domain)
+    if orient_simplices:
+        logger.debug('Orienting simplices...')
+        domain.orient_simplices()
+        logger.debug('Done')
+
     nodes = np.asarray(nodes, dtype=float)
     assert_shape(nodes, (None, domain.dim), 'nodes')
 
     # the `fixed_nodes` are used to provide a repulsion force during
-    # dispersion, but they do not move. TODO There is chance that one of the
-    # points in `fixed_nodes` is equal to a point in `nodes`. This situation
-    # should be handled
+    # dispersion, but they do not move.
     fixed_nodes = np.zeros((0, domain.dim), dtype=float)
     if pinned_nodes is not None:
         pinned_nodes = np.asarray(pinned_nodes, dtype=float)
@@ -340,17 +378,17 @@ def prepare_nodes(nodes, domain,
     if include_vertices:
         fixed_nodes = np.vstack((fixed_nodes, domain.vertices))
 
-    for i in range(iterations):
-        logger.debug(
-            'starting node dispersion iterations %s of %s'
-            % (i + 1, iterations))
+    logger.debug('Dispersing nodes...')
+    nodes = disperse(
+        nodes, domain,
+        iterations=iterations,
+        rho=rho,
+        fixed_nodes=fixed_nodes,
+        neighbors=neighbors,
+        delta=dispersion_delta
+        )
 
-        nodes = disperse(
-            nodes, domain,
-            rho=rho,
-            fixed_nodes=fixed_nodes,
-            neighbors=neighbors,
-            delta=dispersion_delta)
+    logger.debug('Done')
 
     # append the domain vertices to the collection of nodes if requested
     if include_vertices:
@@ -358,15 +396,9 @@ def prepare_nodes(nodes, domain,
 
     # snap nodes to the boundary, identifying which simplex each node
     # was snapped to
-    logger.debug('snapping nodes to boundary ...')
+    logger.debug('Snapping nodes to boundary...')
     nodes, smpid = domain.snap(nodes, delta=snap_delta)
-    logger.debug('done')
-
-    # get the normal vectors for the boundary nodes
-    if orient_simplices:
-        logger.debug('orienting simplices ...')
-        domain.orient_simplices()
-        logger.debug('done')
+    logger.debug('Done')
 
     normals = np.full_like(nodes, np.nan)
     normals[smpid >= 0] = domain.normals[smpid[smpid >= 0]]
@@ -383,46 +415,73 @@ def prepare_nodes(nodes, domain,
         normals = np.vstack((normals, pinned_normals))
         groups['pinned'] = pinned_idx
 
-    if boundary_groups is None:
-        boundary_groups = {'all': range(len(domain.simplices))}
 
-    # TODO: There should be a test to make sure each simplex belongs to at most
-    # one group.
+    logger.debug('Grouping boundary nodes...')
+    if boundary_groups is None:
+        boundary_groups = {'all': np.arange(len(domain.simplices))}
+    else:
+        boundary_groups = {
+            str(k): np.array(v, dtype=int) for k, v in boundary_groups.items()
+            }
+
+        # Validate the user-specified boundary groups
+        simplex_counts = Counter(chain(*boundary_groups.values()))
+        for idx in range(len(domain.simplices)):
+            if simplex_counts[idx] != 1:
+                logger.warning(
+                    'Simplex %s is specified %s times in the boundary groups.'
+                     % (idx, simplex_counts[idx])
+                     )
+
+        extra = set(simplex_counts).difference(range(len(domain.simplices)))
+        if extra:
+            raise ValueError(
+                'The simplex indices %s were specified in the boundary groups '
+                'but do not exist.' % extra
+                )
+
     if boundary_groups_with_ghosts is None:
         boundary_groups_with_ghosts = []
 
-    # create groups for the boundary nodes
-    logger.debug('grouping boundary nodes and generating ghosts ...')
-    for k, v in boundary_groups.items():
-        # convert the list of simplices in the boundary group to a set, because
-        # it is much faster to determine membership of a set
-        v = set(v)
-        bnd_idx = np.array([i for i, j in enumerate(smpid) if j in v])
-        groups['boundary:' + k] = bnd_idx
-        if k in boundary_groups_with_ghosts:
-            # append ghost nodes if requested
-            dist = KDTree(nodes).query(nodes[bnd_idx], 2)[0][:, [1]]
-            ghost_idx = np.arange(bnd_idx.shape[0]) + nodes.shape[0]
-            ghost_nodes = nodes[bnd_idx] + 0.5*dist*normals[bnd_idx]
-            ghost_normals = np.full_like(ghost_nodes, np.nan)
-            nodes = np.vstack((nodes, ghost_nodes))
-            normals = np.vstack((normals, ghost_normals))
-            groups['ghosts:' + k] = ghost_idx
+    # find the mapping from simplex indices to node indices, then use
+    # `boundary_groups` to find which nodes belong to each boundary group
+    smp_to_nodes = [[] for _ in range(len(domain.simplices))]
+    for i, j in enumerate(smpid):
+        if j != -1:
+            smp_to_nodes[j].append(i)
 
-    logger.debug('done')
+    for bnd_name, bnd_smp in boundary_groups.items():
+        bnd_idx = list(chain.from_iterable(smp_to_nodes[i] for i in bnd_smp))
+        groups['boundary:%s' % bnd_name] = np.array(bnd_idx, dtype=int)
 
-    # sort `nodes` so that spatially adjacent nodes are close together
-    logger.debug('sorting nodes ...')
+    logger.debug('Done')
+
+    logger.debug('Creating ghost nodes...')
+    tree = KDTree(nodes)
+    for bnd_name in boundary_groups_with_ghosts:
+        bnd_idx = groups['boundary:%s' % bnd_name]
+        spacing = ghost_delta*tree.query(nodes[bnd_idx], 2)[0][:, 1]
+        ghost_idx = np.arange(bnd_idx.shape[0]) + nodes.shape[0]
+        ghost_nodes = nodes[bnd_idx] + spacing[:, None]*normals[bnd_idx]
+        ghost_normals = np.full_like(ghost_nodes, np.nan)
+        nodes = np.vstack((nodes, ghost_nodes))
+        normals = np.vstack((normals, ghost_normals))
+        groups['ghosts:%s' % bnd_name] = ghost_idx
+
+    logger.debug('Done')
+
+    logger.debug('Sorting nodes...')
     sort_idx = neighbor_argsort(nodes)
-    logger.debug('done')
     nodes = nodes[sort_idx]
     normals = normals[sort_idx]
     reverse_sort_idx = np.argsort(sort_idx)
     groups = {k: reverse_sort_idx[v] for k, v in groups.items()}
+    logger.debug('Done')
 
-    logger.debug('checking the quality of the generated nodes ...')
+
+    logger.debug('Checking the quality of the generated nodes...')
     _check_spacing(nodes, rho)
-    logger.debug('done')
+    logger.debug('Done')
 
     return nodes, groups, normals
 
@@ -533,9 +592,9 @@ def min_energy_nodes(n, domain,
     '''
     domain = as_domain(domain)
     if build_rtree:
-        logger.debug('building R-tree ...')
+        logger.debug('Building R-tree...')
         domain.build_rtree()
-        logger.debug('done')
+        logger.debug('Done')
 
     if rho is None:
         def rho(x):
@@ -605,9 +664,9 @@ def poisson_disc_nodes(radius, domain,
     '''
     domain = as_domain(domain)
     if build_rtree:
-        logger.debug('building R-tree ...')
+        logger.debug('Building R-tree...')
         domain.build_rtree()
-        logger.debug('done')
+        logger.debug('Done')
 
     if np.isscalar(radius):
         scalar_radius = radius
@@ -621,7 +680,8 @@ def poisson_disc_nodes(radius, domain,
     nodes = poisson_discs(
         radius, domain,
         ntests=ntests,
-        rmax_factor=rmax_factor)
+        rmax_factor=rmax_factor
+        )
 
     out = prepare_nodes(nodes, domain, rho=rho, **kwargs)
     return out
