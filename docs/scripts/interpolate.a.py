8c0b87dfb9d69945b0b7dc7320603c4fdf9a8335
Trever Hines <treverhines@gmail.com>
2022-06-05 16:23:11 -0400

    updated readme

--- docs/scripts/interpolate.a.py
+++ docs/scripts/interpolate.a.py
@@ -13,13 +13,14 @@ x_obs = np.random.random((100, 2))
 # values at the observation points
 u_obs = np.sin(2*np.pi*x_obs[:, 0])*np.cos(2*np.pi*x_obs[:, 1]) 
 u_obs += np.random.normal(0.0, 0.1, 100)
-# create a thin-plate spline interpolant, where the data is assumed to
-# be noisy
-I = RBFInterpolant(x_obs, u_obs, sigma=0.1, phi='phs2', order=1)
-# create the interpolation points, and evaluate the interpolant
-x1, x2 = np.linspace(0, 1, 200), np.linspace(0, 1, 200)
-x_itp = np.reshape(np.meshgrid(x1, x2), (2, 200*200)).T
+# Create an RBF interpolant and specify a nonzero value for `sigma` to prevent
+# fitting the noisy data exactly. By setting `phi` to "phs2", the interpolant
+# is equivalent to a thin-plate spline.
+I = RBFInterpolant(x_obs, u_obs, sigma=0.1, phi='phs2')
+# create a grid of evaluation points
+x_itp = np.mgrid[0:1:200j, 0:1:200j].reshape(2, -1).T
 u_itp = I(x_itp) 
+
 # plot the results
 plt.tripcolor(x_itp[:, 0], x_itp[:, 1], u_itp, vmin=-1.1, vmax=1.1, cmap='viridis')
 plt.scatter(x_obs[:, 0], x_obs[:, 1], s=100, c=u_obs, vmin=-1.1, vmax=1.1,
