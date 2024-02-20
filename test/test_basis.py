4333a9254300796cde2fd739f2556c5e2ea8b062
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2024-02-20 11:36:01 +0800

    Add tests for dimensions higher than 15

--- test/test_basis.py
+++ test/test_basis.py
@@ -212,4 +212,20 @@ class Test(unittest.TestCase):
     rbf.basis.phs2(x, y, out=out5)
     self.assertTrue(np.allclose(out1, out5))
 
+    # test dimensions > 15
+    x = np.random.random((5, 16))
+    y = np.random.random((4, 16))
+    out6 = rbf.basis.phs2(x, y)
+
+    out7 = np.empty((5, 4))
+    self.assertRaises(rbf.basis.InplaceError, rbf.basis.phs2, x, y, out=out7)
+
+    out8 = np.empty((5, 4))
+    rbf.basis.phs2(x, y, out=out8, out_compat=True)
+    self.assertTrue(np.allclose(out6, out8))
+
+    out9 = np.empty((4, 5)).T
+    rbf.basis.phs2(x, y, out=out9, out_compat=True)
+    self.assertTrue(np.allclose(out6, out9))
+
 #unittest.main()
